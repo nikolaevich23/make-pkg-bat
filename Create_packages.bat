7d8543f2e0e1d54fcd178b074bb299c57117f34b
nikolaevich23 <in1975@ya.ru>
2018-03-15 13:01:37 +0400

    update bat
    no more args.
    Add change to original path

--- Create_packages.bat
+++ Create_packages.bat
@@ -1,8 +1,10 @@
 @echo off
-set patch=
-set game=%1
-for /d %%d in (B??S?????) do set patch=%%d
+set ptch=
+set game=
+set orig=
+for /d %%d in (B??S?????) do set ptch=%%d
 for /d %%d in (NP???????) do set game=%%d
+for /d %%d in (B??S?????????*) do set orig=%%d
 
 
 if "%game%" NEQ "" (
@@ -18,15 +20,28 @@ if exist %game%\LICDIR xcopy "%game%\LICDIR\*.*" "%game%_LIC\LICDIR\*.*"
 if exist %game%\INSDIR xcopy "%game%\INSDIR\*.*" "%game%_LIC\INSDIR\*.*"
 )
 
-if "%patch%" NEQ "" (
+if "%orig%" NEQ "" (
+echo ContentID = 000000-%orig:~0,9%_00-000000000000GAME> package.conf
+echo Klicensee = 0x00000000000000000000000000000000 >> package.conf
+echo DRMType = Free >> package.conf
+echo ContentType = GameExec >> package.conf
+echo PackageType = HDDGamePatch >> package.conf
+echo InstallDirectory = %orig% >> package.conf
+echo PackageVersion = 01.00 >> package.conf
+psn_package_npdrm.exe -n package.conf %orig%
+if exist %game%\LICDIR xcopy "%game%\LICDIR\*.*" "%game%_LIC\LICDIR\*.*"
+if exist %game%\INSDIR xcopy "%game%\INSDIR\*.*" "%game%_LIC\INSDIR\*.*"
+)
+
+if "%ptch%" NEQ "" (
 echo ContentID = 000000-%game:~0,9%_00-00000000000PATCH > package.conf
 echo Klicensee = 0x00000000000000000000000000000000 >> package.conf
 echo DRMType = Free >> package.conf
 echo ContentType = GameData >> package.conf
 echo PackageType = DiscGamePatch >> package.conf
-echo InstallDirectory = %patch% >> package.conf
+echo InstallDirectory = %ptch% >> package.conf
 echo PackageVersion = 01.00 >> package.conf
-psn_package_npdrm.exe -n package.conf %patch%
+psn_package_npdrm.exe -n package.conf %ptch%
 )
 
 if exist %game%_LIC (
