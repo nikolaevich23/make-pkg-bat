34dd3bf43ab82f6760fba5a73531329686c6ede4
Yaroslav <k4r47ss@gmail.com>
2018-03-20 20:56:05 +0300

    Make the determination of architecture work normal.

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # make-pkg-bat v.1.5.1
-make pkg bat make by [Alex_1985](http://www.pspx.ru/forum/member.php?u=458658) and modify in1975
+make pkg bat made by [Alex_1985](http://www.pspx.ru/forum/member.php?u=458658) and modified by in1975
 
 ### Usage 
-* Put game inside 
-* run make_pkg.bat
+* Put game inside folder
+* Run make_pkg.bat
 	
 ### History
 #### 1.4
-:white_check_mark: add make_pkg.bat (rewrite logic bat file)
+:white_check_mark: Added make_pkg.bat (rewrite logic bat file)
 
-:white_check_mark: add resign_all.bat (test it)
+:white_check_mark: Added resign_all.bat (test it)
 #### 1.5
-:white_check_mark: add new category
+:white_check_mark: Added new category
 
 #### 1.5.1
-:white_check_mark: add color
+:white_check_mark: Added color
--- resign_all.bat
+++ resign_all.bat
@@ -1,26 +1,18 @@
 :: --------------------------------------- ::
 :: Created mod by In1975 for site pspx.ru  ::
+:: Modded by YChvanov                      ::
 :: --------------------------------------- ::
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
 cd /d %~dp0
 
-:: Change these for your application / manual...
-set OS=32
-
-:: Calculate count files
-echo =================================================
-echo Detect OS - %OS%bit.
-echo =================================================
-echo.
-
-:: FUNCTIONS ---------------------------------------------------------------
-
 :getos
   reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32|| set OS=64
+  echo =================================================
+  echo Detected OS architecture - %OS%bit.
+  echo =================================================
+  echo.
 
-for %%f in (*.pkg) do (
-ps3xploit_rifgen_edatresign_x%OS%_mod.exe %%f ps3
-rem del %%f
-)
+if not exist *.pkg (echo No .pkg were found. Exiting... & exit/b)
 
+for %%f in (*.pkg) do (ps3xploit_rifgen_edatresign_x%OS%_mod.exe %%f ps3)
